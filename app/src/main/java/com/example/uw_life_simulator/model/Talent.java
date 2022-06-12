0fc92648cbdd917d62624326268ef934ab4bc4f4
j473xu <j473xu@uwaterloo.ca>
2022-06-11 22:50:58 -0400

    add talent service and activities

---
+++ app/src/main/java/com/example/uw_life_simulator/model/Talent.java
@@ -0,0 +1,67 @@
+package com.example.uw_life_simulator.model;
+
+public class Talent {
+    private int iq;
+    private int luck;
+    private int wealth;
+    private int health;
+
+    // Constructor
+    public Talent() {
+        this.iq = 0;
+        this.luck = 0;
+        this.wealth = 0;
+        this.health = 0;
+    }
+
+    // Constructor
+    public Talent(int iq, int luck, int wealth, int health) {
+        this.iq = iq;
+        this.luck = luck;
+        this.wealth = wealth;
+        this.health = health;
+    }
+
+    @Override
+    public String toString() {
+        return "Talent{" +
+                "iq=" + iq +
+                ", luck=" + luck +
+                ", wealth=" + wealth +
+                ", health=" + health +
+                '}';
+    }
+
+
+    public int getIq() {
+        return iq;
+    }
+
+    public void setIq(int iq) {
+        this.iq = iq;
+    }
+
+    public int getLuck() {
+        return luck;
+    }
+
+    public void setLuck(int luck) {
+        this.luck = luck;
+    }
+
+    public int getWealth() {
+        return wealth;
+    }
+
+    public void setWealth(int wealth) {
+        this.wealth = wealth;
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    public void setHealth(int health) {
+        this.health = health;
+    }
+}
