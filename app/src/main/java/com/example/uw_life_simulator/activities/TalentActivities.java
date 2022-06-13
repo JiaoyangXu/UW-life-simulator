1100b4f4bfe9d0b1e309e28a1bb9b3d6f50afa42
j473xu <j473xu@uwaterloo.ca>
2022-06-12 21:12:48 -0400

    update IQ buttons and its activity listener

--- app/src/main/java/com/example/uw_life_simulator/activities/TalentActivities.java
+++ app/src/main/java/com/example/uw_life_simulator/activities/TalentActivities.java
@@ -2,8 +2,12 @@ package com.example.uw_life_simulator.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
+import com.example.uw_life_simulator.MainActivity;
 import com.example.uw_life_simulator.R;
 import com.example.uw_life_simulator.model.Talent;
 
@@ -16,5 +20,31 @@ public class TalentActivities extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activities_talent_selection);
     }
+
+    // Increment IQ if the addIQ button is hit
+    public void addIQ(View view) {
+        // get current IQ amount
+        TextView textView = findViewById(R.id.talentIQAmount);
+        String IQString = textView.getText().toString();
+        int IQAmount = Integer.parseInt(IQString);
+        // increment IQ amount
+        IQAmount++;
+        textView.setText(String.valueOf(IQAmount));
+    }
+
+    // decrease IQ if the minusIQ button is hit
+    public void minusIQ(View view) {
+        // get current IQ amount
+        TextView textView = findViewById(R.id.talentIQAmount);
+        String IQString = textView.getText().toString();
+        int IQAmount = Integer.parseInt(IQString);
+        // decrease IQ amount
+        IQAmount = Math.max(--IQAmount,0);
+        textView.setText(String.valueOf(IQAmount));
+    }
+
+
+
+
 
 }
