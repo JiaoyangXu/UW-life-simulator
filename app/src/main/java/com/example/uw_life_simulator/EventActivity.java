da2995dbc16440b5e8a54eea99d4e9f665b26478
Jiayi Cai <karencai1521816@gmail.com>
2022-06-12 22:19:41 -0400

    create event activity

---
+++ app/src/main/java/com/example/uw_life_simulator/EventActivity.java
@@ -0,0 +1,29 @@
+package com.example.uw_life_simulator;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.example.uw_life_simulator.databinding.ActivityMainBinding;
+
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class EventActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.event_list_page);
+
+    }
+
+
+}
