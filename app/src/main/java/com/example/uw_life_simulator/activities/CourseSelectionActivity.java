4e3e1f406d04ac06cfa2ed6cb8294d7c28d34264
j473xu <j473xu@uwaterloo.ca>
2022-06-21 15:51:54 -0400

    created courseSelection activity in AndroidManifest.xml

--- app/src/main/java/com/example/uw_life_simulator/activities/CourseSelectionActivity.java
+++ app/src/main/java/com/example/uw_life_simulator/activities/CourseSelectionActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.example.uw_life_simulator.R;
+
 public class CourseSelectionActivity extends AppCompatActivity {
 
     @Override
