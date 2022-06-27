d29fe3438cbedb42b4ac7301d112feeb929d21aa
Echo Liu <echoxiaobai@gmail.com>
2022-06-26 20:04:11 -0400

    Could generate new events

--- app/src/main/java/com/example/uw_life_simulator/model/NewEvent.java
+++ app/src/main/java/com/example/uw_life_simulator/model/NewEvent.java
@@ -1,9 +1,17 @@
 package com.example.uw_life_simulator.model;
 
+import com.example.uw_life_simulator.data.PlayerAttribute;
+import com.example.uw_life_simulator.model.Factory;
+
 public class NewEvent {
     public static String generateNewEvent(Player player, int totalnum)
     {
-        String s = "New Event" + totalnum + "is: Welcome to UWaterloo!";
+        GameEvent gm = Factory.generateEvent(totalnum % 4);
+        //String s = "New Event " + totalnum + " is: " + gm.description;
+        String s = gm.description;
+        PlayerAttribute pa = player.getPlayerAttribute();
+        pa.GPA -= 1;
+        player.setPlayerAttribute(pa);
         return s;
     }
 }
