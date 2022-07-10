be49bdd56390573a28aa8943bca8f0fb8b26fcc3
Jason Xu <jasonxu@v1040-wn-rt-a-96-121.campus-dynamic.uwaterloo.ca>
2022-07-09 22:57:04 -0400

    add page flipping feature

--- app/src/main/java/com/example/uw_life_simulator/activities/DrawSpellCardActivity.java
+++ app/src/main/java/com/example/uw_life_simulator/activities/DrawSpellCardActivity.java
@@ -8,6 +8,7 @@ import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.View;
 import android.view.animation.LinearInterpolator;
+import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.TextView;
 
@@ -27,13 +28,18 @@ import java.util.Collections;
 import java.util.List;
 
 public class DrawSpellCardActivity extends AppCompatActivity {
+    Button nextCard, prevCard;
     InfiniteCardView cardStack;
     CardAdapter cardAdapter;
     List<Integer> card_list = new ArrayList<>();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_draw_spell_card);
+
+        nextCard = (Button) findViewById(R.id.nextCard);
+        prevCard = (Button) findViewById(R.id.prevCard);
 
         card_list.add(R.drawable.img);
         card_list.add(R.drawable.img_1);
@@ -44,7 +50,7 @@ public class DrawSpellCardActivity extends AppCompatActivity {
 
         cardStack = (InfiniteCardView) findViewById(R.id.view);
 
-        //cardStack.setClickable(true);
+        cardStack.setClickable(true);
 
         cardStack.setAnimType(InfiniteCardView.ANIM_TYPE_FRONT);
         cardStack.setAnimInterpolator(new LinearInterpolator());
@@ -53,10 +59,23 @@ public class DrawSpellCardActivity extends AppCompatActivity {
         cardStack.setZIndexTransformerToBack(new DefaultZIndexTransformerCommon());
         cardStack.setAdapter(cardAdapter);
 
-        for (int i = 0; i < 10; i++) {
-            cardStack.bringCardToFront(i);
-        }
 
+        nextCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                cardStack.bringCardToFront(1);
+            }
+        });
+
+        prevCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                cardStack.bringCardToFront(cardAdapter.getCount() - 1);
+            }
+        });
+
+        //prevCard.callOnClick();
     }
+
 }
 
