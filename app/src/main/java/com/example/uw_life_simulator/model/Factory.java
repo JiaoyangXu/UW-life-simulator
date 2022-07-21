ba972826c13c648cbd5dd27038c8667cbe6cd2d1
Haoyu Yin <h48yin@uwaterloo.ca>
2022-07-21 17:23:49 -0400

    deleted Archived files

--- app/src/main/java/com/example/uw_life_simulator/model/Factory.java
+++ app/src/main/java/com/example/uw_life_simulator/model/Factory.java
@@ -128,18 +128,6 @@ public class Factory {
     }
 
     /**
-     * NewEvent generator which generates events based on the eventId
-     * Events are implemented in GameEventGenerator.Java
-     *
-     * Input: int : eventId
-     * Output: GameEvent : Generated event
-     **/
-    static public GameEvent generateEvent(int eventId) // Implementation needed
-    {
-        return GameEventGenerator.generateEvent(eventId);
-    }
-
-    /**
      * Talent generator which generates events based on the talentId
      * Talents are implemented in TalentGenerator.Java
      *
@@ -150,30 +138,6 @@ public class Factory {
     {
         return BuffGenerator.generateBuff(buffId);
     }
-
-    /**
-     * Company generator which generates events based on the companyId
-     * Company info is stored in database file
-     *
-     * Input: int : companyId
-     * Output: Company : Generated company
-     **/
-    static public Company generateCompany(int companyId) // Implementation needed
-    {
-        return null;
-    }
-
-    /**
-     * Course generator which generates events based on the courseId
-     * Course info is stored in database file
-     *
-     * Input: int : courseId
-     * Output: Course : Generated course
-     **/
-    /*static public Course generateCourse(int courseId) // Implementation needed
-    {
-        return null;
-    }*/
 }
 
 class RandomEventListCommon
